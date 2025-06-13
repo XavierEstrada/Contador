4d5acdd97a241a7cc5ef5e80ec0d09c568b2fabd
XGuadamuz <xguadamuz70935@ufide.ac.cr>
2025-06-13 12:50:26 -0600

    commit #1

---
+++ contador.js
@@ -0,0 +1,24 @@
+
+const fechaFinal = new Date("2025-07-10T14:10:00").getTime();
+
+function actualizarContador() {
+  const ahora = new Date().getTime();
+  const diferencia = fechaFinal - ahora;
+
+  const contador = document.getElementById("contador");
+
+  if (diferencia <= 0) {
+    contador.innerText = "¡Disfrutemos nuestro tiempo juntos ❤!";
+    clearInterval(intervalo);
+    return;
+  }
+
+  const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+  const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+  contador.innerText = `${dias}d ${horas}h ${minutos}m ${segundos}s`;
+}
+
+const intervalo = setInterval(actualizarContador, 1000);
